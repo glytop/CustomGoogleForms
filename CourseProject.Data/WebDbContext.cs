07b2975b4df0f65d0f84102191d31b18d5cb4920
glytop <123397208+glytop@users.noreply.github.com>
2025-04-03 17:46:50 +0300

    Initial

---
+++ CourseProject.Data/WebDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CourseProject.Data
+{
+    public class WebDbContext : DbContext
+    {
+        public WebDbContext(DbContextOptions<WebDbContext> contextOptions) : base(contextOptions)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
